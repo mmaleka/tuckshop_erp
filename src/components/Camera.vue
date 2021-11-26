cf488007620aaae8befb73a1ec885dda14931284
Mpho Maleka <MMaleka@rdm.local>
2021-11-26 14:25:39 +0200

    auto commit

--- src/components/Camera.vue
+++ src/components/Camera.vue
@@ -90,46 +90,46 @@ export default {
 }
 </script>
 
-// <style lang="scss" scoped>
-// .camera {
-//     width: 100vw;
-//     height: 100vh;
-//     padding: 25px;
-//     box-sizing: border-box;
+<style lang="scss" scoped>
+.camera {
+    width: 100vw;
+    height: 100vh;
+    padding: 25px;
+    box-sizing: border-box;
     
     
-//     .feed {
-//         display: black;
-//         width: 100%;
-//         max-width:1280px;
+    .feed {
+        display: black;
+        width: 100%;
+        max-width:1280px;
 
-//         margin: 0 auto;
+        margin: 0 auto;
 
-//         background-color: #171717;
-//         box-shadow: 6px 6px 12px 8px rgba(0, 0, 0, 0.35); 
-//     }
+        background-color: #171717;
+        box-shadow: 6px 6px 12px 8px rgba(0, 0, 0, 0.35); 
+    }
 
-//     .snap {
-//         display: block;
-//         width: 75px;
-//         height: 75px;
-//         border-radius: 50%;
+    .snap {
+        display: block;
+        width: 75px;
+        height: 75px;
+        border-radius: 50%;
 
-//         margin: 25px auto;
+        margin: 25px auto;
 
-//         background-color: transparentize($color: #ffce00, $amount: 0.5);
-//         border: 1px solid #171717;
-//         outline: none;
+        background-color: transparentize($color: #ffce00, $amount: 0.5);
+        border: 1px solid #171717;
+        outline: none;
 
-//         cursor: pointer;
+        cursor: pointer;
 
-//         &:hover {
-//             background-color: #ffce00;
-//         }
-//         &:active {
-//             background-color: darken($color: #ffce00, $amount: 10);
-//         }
-//     }
+        &:hover {
+            background-color: #ffce00;
+        }
+        &:active {
+            background-color: darken($color: #ffce00, $amount: 10);
+        }
+    }
 
-// }
-// </style>
+}
+</style>
